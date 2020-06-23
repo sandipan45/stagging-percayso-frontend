9525a58cbc25f2a524538260a92662a5e1ebfde2
sandipan45 <sandipan45@gmail.com>
2020-06-23 22:49:33 +0530

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
 	{
 	resolve: `gatsby-source-strapi`,
 		options: {
-			apiURL: process.env.API_URL || `https://stagging-percayso-backend.herokuapp.com:1337`,
+			apiURL: process.env.API_URL || `http://localhost:1337`,
 			queryLimit: 1000, // Default to 100
 			contentTypes: ['article'],
 			singleTypes: []
